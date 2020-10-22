340384a29dfd819782ed0585be016bb74e01cc86
James Lambert <jameslambert@jameslambert.office.lucidchart.com>
2020-10-22 16:56:34 -0600

    Work on aiff serialize

--- aiff/aiffserialize.go
+++ aiff/aiffserialize.go
@@ -3,11 +3,98 @@ package aiff
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"io"
 )
 
-func (commonChunk *CommonChunk) serialize() (*bytes.Buffer, error) {
+func writePString(out io.Writer, val string) error {
+	var byteLen = len(val)
+
+	if byteLen >= 256 {
+		return errors.New("Too long for PString")
+	} else {
+		var len uint8 = uint8(byteLen)
+
+		err := binary.Write(out, binary.BigEndian, &len)
+
+		if err != nil {
+			return err
+		}
+
+		asBytes := []byte(val)
+
+		_, err = out.Write(asBytes)
+
+		if err != nil {
+			return err
+		}
+
+		// check for padding byte
+		if len&0x1 == 0x0 {
+			len = 0
+			err := binary.Write(out, binary.BigEndian, &len)
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func pStringLen(val string) uint32 {
+	var result = 1 + uint32(len(val))
+
+	if result&0x1 == 0x1 {
+		return result + 1
+	} else {
+		return result
+	}
+}
+
+func writeExtendedFloat(out io.Writer, exFloat *ExtendedFloat) {
+	var expSign uint16 = exFloat.Exponent
+
+	if exFloat.Sign {
+		expSign = expSign | 0x8000
+	}
+
+	binary.Write(out, binary.BigEndian, &exFloat)
+	binary.Write(out, binary.BigEndian, &exFloat.Mantissa)
+}
+
+func (commonChunk *CommonChunk) serialize(compressed bool) (*bytes.Buffer, error) {
 	var result bytes.Buffer
+
+	var header uint32 = COMM
+	err := binary.Write(&result, binary.BigEndian, &header)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var chunkLen uint32
+
+	if compressed {
+		chunkLen = 22 + pStringLen(commonChunk.CompressionName)
+	} else {
+		chunkLen = 18
+	}
+
+	err = binary.Write(&result, binary.BigEndian, &chunkLen)
+
+	if err != nil {
+		return nil, err
+	}
+
+	binary.Write(&result, binary.BigEndian, &commonChunk.NumChannels)
+	binary.Write(&result, binary.BigEndian, &commonChunk.NumSampleFrames)
+	binary.Write(&result, binary.BigEndian, &commonChunk.SampleSize)
+
+	writeExtendedFloat(&result, &commonChunk.SampleRate)
+
+	if compressed {
+		binary.Write(&result, binary.BigEndian, &commonChunk.CompressionType)
+		err = writePString(&result, commonChunk.CompressionName)
+	}
 
 	return &result, nil
 }
@@ -15,7 +102,7 @@ func (commonChunk *CommonChunk) serialize() (*bytes.Buffer, error) {
 func (aiff *Aiff) Serialize(writer io.Writer) error {
 	var bufferChunks []*bytes.Buffer
 
-	commonChunk, err := aiff.Common.serialize()
+	commonChunk, err := aiff.Common.serialize(aiff.Compressed)
 
 	if err != nil {
 		return err
@@ -42,8 +129,24 @@ func (aiff *Aiff) Serialize(writer io.Writer) error {
 		return err
 	}
 
-	for _, chunk := range bufferChunks {
+	if aiff.Compressed {
+		header = AIFC
+	} else {
+		header = AIFF
+	}
 
+	err = binary.Write(writer, binary.BigEndian, &header)
+
+	if err != nil {
+		return err
+	}
+
+	for _, chunk := range bufferChunks {
+		_, err := writer.Write(chunk.Bytes())
+
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
