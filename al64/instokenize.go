3befe67c621ccd5b0b55b0acd795f8405e65d9be
James Lambert <jameslambert@jameslambert.office.lucidchart.com>
2020-11-16 16:30:04 -0700

    Finish up tokenizer states

--- al64/instokenize.go
+++ al64/instokenize.go
@@ -31,11 +31,11 @@ type tokenizer struct {
 }
 
 type token struct {
-	value     string
-	line      int
-	start     int
-	end       int
-	tokenType tokenType
+	value string
+	line  int
+	start int
+	end   int
+	tType tokenType
 }
 
 func tokenizeIdentifier(next rune) (tokenType, tokenizeState) {
@@ -58,8 +58,94 @@ func tokenizeError(next rune) (tokenType, tokenizeState) {
 	return tokenTypeError, tokenizeDefaultState(next)
 }
 
+func singleToken(tType tokenType) tokenizeState {
+	return func(next rune) (tokenType, tokenizeState) {
+		return tType, tokenizeDefaultState(next)
+	}
+}
+
+func tokenizeString(next rune) (tokenType, tokenizeState) {
+	if next == '\\' {
+		return tokenTypeNone, tokenizeStringEscape
+	} else if next == '"' {
+		return tokenTypeString, tokenizeDefaultState(next)
+	} else {
+		return tokenTypeNone, tokenizeString
+	}
+}
+
+func tokenizeStringEscape(next rune) (tokenType, tokenizeState) {
+	return tokenTypeNone, tokenizeString
+}
+
+func tokenizeCommentSingle(next rune) (tokenType, tokenizeState) {
+	if next == '\n' {
+		return tokenTypeComment, tokenizeWhitespace
+	} else {
+		return tokenTypeNone, tokenizeCommentSingle
+	}
+}
+
+func tokenizeCommentMulti(next rune) (tokenType, tokenizeState) {
+	if next == '*' {
+		return tokenTypeNone, tokenizeCommentMultiEnd
+	} else {
+		return tokenTypeNone, tokenizeCommentMulti
+	}
+}
+
+func tokenizeCommentMultiEnd(next rune) (tokenType, tokenizeState) {
+	if next == '/' {
+		return tokenTypeComment, tokenizeDefaultState(next)
+	} else if next == '*' {
+		return tokenTypeNone, tokenizeCommentMultiEnd
+	} else {
+		return tokenTypeNone, tokenizeCommentMulti
+	}
+}
+
+func tokenizeCommentStart(next rune) (tokenType, tokenizeState) {
+	if next == '/' {
+		return tokenTypeNone, tokenizeCommentSingle
+	} else if next == '*' {
+		return tokenTypeNone, tokenizeCommentMulti
+	} else {
+		return tokenTypeError, tokenizeDefaultState(next)
+	}
+}
+
+func tokenizeWhitespace(next rune) (tokenType, tokenizeState) {
+	if unicode.IsSpace(next) {
+		return tokenTypeNone, tokenizeWhitespace
+	} else {
+		return tokenTypeWhitespace, tokenizeDefaultState(next)
+	}
+}
+
 func tokenizeDefaultState(next rune) tokenizeState {
-	if unicode.IsLetter(next) {
+	if next == '{' {
+		return singleToken(tokenTypeOpenCurly)
+	} else if next == '}' {
+		return singleToken(tokenTypeCloseCurly)
+	} else if next == '(' {
+		return singleToken(tokenTypeOpenParen)
+	} else if next == ')' {
+		return singleToken(tokenTypeCloseParen)
+	} else if next == '[' {
+		return singleToken(tokenTypeOpenSquare)
+	} else if next == ']' {
+		return singleToken(tokenTypeCloseSquare)
+	} else if next == '=' {
+		return singleToken(tokenTypeEqual)
+	} else if next == ';' {
+		return singleToken(tokenTypeSemiColon)
+	} else if next == '"' {
+		return tokenizeString
+	} else if next == '/' {
+		return tokenizeCommentStart
+	} else if unicode.IsSpace(next) {
+		return tokenizeWhitespace
+	} else if unicode.IsLetter(next) {
 		return tokenizeIdentifier
 	} else if unicode.IsDigit(next) {
 		return tokenizeNumber
